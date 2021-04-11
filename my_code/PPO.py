7fd28006413a4394bd06829cb488ac2155bd8447
nube266 <tennpi266@gmail.com>
2021-04-11 19:10:26 +0900

    Complete simple PPO model

--- my_code/PPO.py
+++ my_code/PPO.py
@@ -1,6 +1,8 @@
 import torch
+import torch.nn as nn
 from torch.distributions import MultivariateNormal
 from network import FeedForwardNN
+from torch.optim import Adam
 
 
 class PPO:
@@ -18,6 +20,10 @@ class PPO:
         self.actor = FeedForwardNN(self.obs_dim, self.act_dim)
         self.critic = FeedForwardNN(self.obs_dim, 1)
 
+        # Optimizerの初期化
+        self.actor_optim = Adam(self.actor.parameters(), lr=self.lr)
+        self.actor_critic = Adam(self.critic.parameters(), lr=self.lr)
+
         # 共分散行列の作成
         self.cov_var = torch.full(size=(self.act_dim,), fill_value=0.5)
         self.cov_mat = torch.diag(self.cov_var)
@@ -27,6 +33,51 @@ class PPO:
         while t_so_far < total_timesteps:  # ALG STEP 2
             # ALG STEP 3
             batch_obs, batch_acts, batch_log_probs, batch_rtgs, batch_lens = self.rollout()
+
+            # このバッチを収集したタイムステップ数を算出
+            t_so_far += np.sum(batch_lens)
+
+            # V_{phi_k}を算出
+            V, _ = self.evaluate(batch_obs, batch_acts)
+
+            # ALG STEP 5
+            # advantageの算出と正規化
+            A_k = batch_rtgs - V.detach()
+            A_k = (A_k - A_k.mean()) / (A_k.std() + 1e-10)
+
+            for _ in range(self.n_updates_per_iteration):
+                # pi_theta(a_t | s_t)を計算
+                V, curr_log_probs = self.evaluate(batch_obs, batch_acts)
+
+                # 比率を計算する
+                ratios = torch.exp(curr_log_probs - batch_log_probs)
+
+                # 代理損失(サロゲート損失)を算出
+                surr1 = ratios * A_k
+                surr2 = torch.clamp(ratios, 1 - self.clip, 1 + self.clip) * A_k
+
+                # 損失を算出
+                actor_loss = (-torch.min(surr1, surr2)).mean()
+                critic_loss = nn.MSELoss()(V, batch_rtgs)
+
+                # 勾配を算出 & Actorネットワークのバックプロバゲーション
+                self.actor_optim.zero_grad()
+                actor_loss.backward()
+                self.actor_optim.step()
+
+                # 勾配を算出 & Criticネットワークのバックプロバゲーション
+                self.critic_optim.zero_grad()
+                critic_loss.backward()
+                self.critic_optim.step()
+
+    def evaluate(self, batch_obs, batch_acts):
+        # 各観測値のVについてcriticネットワークに問い合わせる
+        V = self.critic(batch_obs).squeeze()
+        # 最新のアクターネットワークを使用して対数確率を算出
+        mean = self.actor(batch_obs)
+        dist = MultivariateNormal(mean, self.cov_mat)
+        log_probs = dist.log_prob(batch_acts)
+        return V, log_probs
 
     def rollout(self):
         # バッチデータ
@@ -99,3 +150,13 @@ class PPO:
         self.timesteps_per_batch = 4800  # バッチあたりのタイムステップ
         self.max_timesteps_per_episode = 1600  # エピソードあたりのタイムステップ
         self.gamma = 0.95  # 割引率
+        self.n_updates_per_iteration = 5  # イテレーションあたりのエポック
+        self.clip = 0.2  # クリップのしきい値
+        self.lr = 0.005  # 学習率
+
+
+if __name__ == '__main__':
+    import gym
+    env = gym.make('Pendulum-v0')
+    model = PPO(env)
+    model.learn(10000)
